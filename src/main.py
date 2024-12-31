4c208bb433e7be00b951c19c8c859c7967fe7e24
SianiEmanuele <sianiemanuele@gmail.com>
2024-12-31 16:52:05 +0100

    + Created tracker.py class + Added YOLOv9 training + Added bbox utils + Added bounding box utils for taking coordinates

--- src/main.py
+++ src/main.py
@@ -1,11 +1,22 @@
+from dotenv import load_dotenv
 from utils import read_video, save_video
+from trackers import Tracker
+
+load_dotenv()
+
 
 def main():
     video_frames = read_video(r"../input_videos/08fd33_4.mp4")
-    print(video_frames)
+    tracker = Tracker(model_path=r"training/runs/detect/train7/weights/best.pt")
+
+    tracks = tracker.get_object_tracks(video_frames)
+
+    # Draw output
+    ## Draw object tracks
+    output_video_frames = tracker.draw_annotations(video_frames, tracks)
 
 
-    save_video(video_frames, r"../output_videos/output.avi")
+    save_video(output_video_frames, r"../output_videos/output.avi")
 
 
 if __name__ == '__main__':
